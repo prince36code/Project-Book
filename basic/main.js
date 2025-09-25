1246cedd21512781af27bf5dcbc284e9db19480e
prince murinda <princetichatonga25@gmail.com>
2025-09-25 16:00:15 +0530

    prince

---
+++ basic/main.js
@@ -0,0 +1,35 @@
+ import * as THREE from 'three';
+
+ //1 . create  the scene
+ const scene =  new THREE.Scene();
+ scene.background = new THREE.Color('#1033c0ff');
+
+ // 2.Add the camera
+ const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+ camera.position.z = 5;
+ // 3. Create and add a cube object
+ const geometry = new THREE.BoxGeometry();
+ const material = new THREE.MeshLambertMaterial({color: '#468585',emissive: '#468585'})
+
+ const cube = new THREE.Mesh(geometry,material);
+ scene.add(cube);
+ // 4.Add lighting
+ const light = new THREE.DirectionalLight(0x09cba6,10);
+ light.position.set(1,1,1);
+ scene.add(light);
+ //5. Set up the renderer
+ const renderer = new THREE.WebGLRenderer();
+ renderer.setSize(window.innerWidth,window.innerHeight);
+ document.body.appendChild(renderer.domElement);
+ //6. Animate the scene
+ function animate(){
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
+    cube.rotation.z += 0.1;
+    renderer.render(scene,camera);
+ }
+ animate();
+ 
+ 
