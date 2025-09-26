ac7736d02cc0d46788c2057b8593da6edb39f791
prince murinda <princetichatonga25@gmail.com>
2025-09-26 11:41:02 +0530

    three js

---
+++ vite/main1.js
@@ -0,0 +1,61 @@
+
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const canvas = document.getElementById("canvas");
+
+//1.scene
+
+const scene  = new THREE.Scene();
+scene.background  = new THREE.Color('#000080');
+
+//2.camera
+
+const camera = new THREE.PerspectiveCamera( 75,  window.innerWidth/ window.innerHeight,0.1,1000);
+camera.position.z = 5;
+
+//3.object
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshLambertMaterial({ color: '#468585', emissive: '#468585' });
+const dodecahedron= new THREE.Mesh(geometry,material);
+
+const boxGeometry = new THREE.BoxGeometry(2,0.1,2);
+const boxmaterial = new THREE.MeshLambertMaterial({color:'#B4B4B3',emissive:'#B4B4B3'});
+const box = new THREE.Mesh(boxGeometry,boxmaterial);
+
+box.position.y = -1.5;
+
+scene.add(dodecahedron);
+scene.add(box);
+
+// light
+const light = new THREE.SpotLight(0x006769,100);
+light.position.set(1,1,1);
+scene.add(light);
+
+//5.renderer
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio); 
+
+//6. Add orbitControls
+const controls = new OrbitControls(camera,renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+// add animations
+
+function animate(){
+    requestAnimationFrame(animate);
+    dodecahedron.rotation.x += 0.01;
+    dodecahedron.rotation.y += 0.01;
+
+    box.rotation.y += 0.005;
+    controls.update();
+    renderer.render(scene,camera);
+
+}
+
+animate();
+
